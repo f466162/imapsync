c9cb220ff83580b8884deb3215a54a110435c4e4
Fabian Gorsler <fabian.gorsler@gmx.de>
2020-05-03 19:14:24 +0200

    Added some comments and improvements

--- imapsync.sh
+++ imapsync.sh
@@ -24,34 +24,51 @@ function doSync() {
          > $LOGFILE 2>&1
 }
 
+# We stay in background
+renice -n 19 $$ > /dev/null
+
+# Just exit in case of signals
 trap 'exit 0' SIGTERM
 trap 'exit 0' SIGKILL
 
+# Control loop
 while true
 do
+	# Account loop
 	for i in $HOME/*.conf
 	do
+		# Might happen in case of an empty volume or wrong permissions
 		if ! [ -r "$i" ]
 		then
-			>&2 echo Fehler beim Einlesen von $i
+			>&2 echo Error reading configuration from $i
 			exit 1
 		fi
 
-		. $i
-		export LOGFILE=$HOME/`basename $i .conf`.log
+		# Load config file
+		. ${i}
 
-		echo INFO: Start $i
+		# Determine log file
+		export LOGFILE=$HOME/`basename ${i} .conf`.log
+
+		# Clean old log
+		truncate -s 0 ${LOGFILE}
+
+		echo INFO: Sync startet for ${i}
 
 		doSync || {
-			>&2 echo Fehler bei $i aufgetreten
-			cp $LOGFILE $LOGFILE.$$.`date +%s`		
+			errorlog=${LOGFILE}.$$.`date +%s`
+			>&2 echo An error occured for ${i}, see ${errorlog}
+
+			cp $LOGFILE ${errorlog}
+
 			exit 1
 		}
 
 		egrep "^(Transfer time|Folders synced|Messages transferred)" $LOGFILE
-		echo INFO: Ende $i
+		echo INFO: Sync ended for ${i}
 	done
 
+	# Wait for next sync
 	sleep $(((RANDOM % 1337) + 60))
 done
 
